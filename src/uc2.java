12b9ef8844c4556f97442323e75e59aecc1b2093
devendra-99-dev <devendrakumarchitturi01@gmail.com>
2026-02-18 01:41:18 +0530

    Added uc2 program

---
+++ src/uc2.java
@@ -0,0 +1,12 @@
+public class uc2{
+    public static void main(String[] args){
+        //Uc2:
+        System.out.println("   ***   " + " " + "   *****  ");
+        System.out.println(" **   ** " + " " + " **       ");
+        System.out.println("**     **" + " " + "**        ");
+        System.out.println("**     **" + " " + "  *****   ");
+        System.out.println("**     **" + " " + "      **  ");
+        System.out.println(" **   ** " + " " + " **   **  ");
+        System.out.println("   ***   " + " " + "   *****  ");
+    }
+}
